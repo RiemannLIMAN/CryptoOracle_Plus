5f794edbc34ebbd226dcdb943f97443bd56d2da2
DengTaoNiu <niudtao@163.com>
2026-01-02 01:29:09 +0800

    [v3.4.4 (Trailing Stop & Sniper Recovery)]

--- OKXBot_Plus_Workspace/src/OKXBot_Plus.py
+++ OKXBot_Plus_Workspace/src/OKXBot_Plus.py
@@ -16,7 +16,7 @@ from services.strategy.ai_strategy import DeepSeekAgent
 from services.execution.trade_executor import DeepSeekTrader
 from services.risk.risk_manager import RiskManager
 
-SYSTEM_VERSION = "v3.4.4  (Trailing Stop & Sniper Recovery)"
+SYSTEM_VERSION = "v3.4.4 (Trailing Stop & Sniper Recovery)"
 
 BANNER = r"""
    _____                  __           ____                  __   
